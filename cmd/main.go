ad90fef3624d4e8e91265df7b915f597c510ee6b
Alexander Novikov <nvkv_ai@outlook.com>
2024-02-26 14:17:51 +0300

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"tdd-based-app/internal/server"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerStore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	fmt.Println("recording...")
-}
-
 func main() {
-	s := server.New(&InMemoryPlayerStore{})
+	s := server.New(server.NewInMemoryPlayersStore())
 
 	log.Fatal(http.ListenAndServe(":4000", s))
 }

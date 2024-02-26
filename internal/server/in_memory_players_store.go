227e73d166d97d107b6cae9b99a964d9471f7657
Alexander Novikov <nvkv_ai@outlook.com>
2024-02-26 14:26:11 +0300

    add pre-post conditions

--- internal/server/in_memory_players_store.go
+++ internal/server/in_memory_players_store.go
@@ -4,20 +4,25 @@ type InMemoryPlayerStore struct {
 	store map[string]int
 }
 
+// Postcondition: returns a new empty in-memory storage
 func NewInMemoryPlayersStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{
 		make(map[string]int),
 	}
 }
 
+// Postcondition: increment total wins of given player (new player created if not exists)
 func (s *InMemoryPlayerStore) RecordWin(name string) {
 	s.store[name]++
 }
 
-func (s *InMemoryPlayerStore) GetPlayerStore(name string) int {
+// Precondition: given player exists (if not - returns 0)
+// Postcondition: returns a total score of given player
+func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return s.store[name]
 }
 
+// Postcondition: returns a list of all players with total score more than 0
 func (s *InMemoryPlayerStore) GetPlayers() []string {
 	result := []string{}
 

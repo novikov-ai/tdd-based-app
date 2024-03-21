c887323ceddd9da6ab63ef61620bd86fe2dbb5bf
Alexander Novikov <nvkv_ai@outlook.com>
2024-03-21 10:32:37 +0300

    init stub for new feature

--- internal/server/in_memory_players_store.go
+++ internal/server/in_memory_players_store.go
@@ -1,19 +1,23 @@
 package server
 
 type InMemoryPlayerStore struct {
-	store map[string]int
+	store          map[string]int
+	playersLeagues map[string]string
 }
 
 // Postcondition: returns a new empty in-memory storage
 func NewInMemoryPlayersStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{
-		make(map[string]int),
+		store:          make(map[string]int),
+		playersLeagues: make(map[string]string),
 	}
 }
 
-// Postcondition: increment total wins of given player (new player created if not exists)
-func (s *InMemoryPlayerStore) RecordWin(name string) {
+// Postcondition: increment total wins of given player (new player created if not exists);
+// player inserted with a given league
+func (s *InMemoryPlayerStore) RecordWin(name, league string) {
 	s.store[name]++
+	s.playersLeagues[name] = league
 }
 
 // Precondition: given player exists (if not - returns 0)
@@ -31,4 +35,12 @@ func (s *InMemoryPlayerStore) GetPlayers() []string {
 	}
 
 	return result
+}
+
+// Precondition: given league exists
+// Postcondition: returns all players of given league
+func (s *InMemoryPlayerStore) GetPlayersOfLeague(league string) []string {
+	// TODO: implement here
+
+	return []string{}
 }

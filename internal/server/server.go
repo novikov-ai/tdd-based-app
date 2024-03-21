c1e5e312ce387c91a700bca677165a808908d957
Alexander Novikov <nvkv_ai@outlook.com>
2024-03-21 23:04:28 +0300

    register handler with players league

--- internal/server/server.go
+++ internal/server/server.go
@@ -39,6 +39,11 @@ func (ps *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		ps.processLeague(w, r)
 	}))
 
+	// register a new endpoint `/players/league?value=`
+	router.Handle("/players/league", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ps.processLeaguePlayers(w, r)
+	}))
+
 	// server serves all registered endpoints
 	router.ServeHTTP(w, r)
 }
@@ -47,9 +52,12 @@ func (ps *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (ps *PlayerServer) processPlayers(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
+	query := r.URL.Query()
+	league := query.Get("league")
+
 	switch r.Method {
 	case http.MethodPost:
-		ps.recordWin(w, player)
+		ps.recordWin(w, player, league)
 	case http.MethodGet:
 		ps.showScore(w, player)
 	}
@@ -69,8 +77,8 @@ func (ps *PlayerServer) processLeague(w http.ResponseWriter, r *http.Request) {
 	w.Write(bytes)
 }
 
-func (ps *PlayerServer) recordWin(w http.ResponseWriter, player string) {
-	ps.store.RecordWin(player, "")
+func (ps *PlayerServer) recordWin(w http.ResponseWriter, player, league string) {
+	ps.store.RecordWin(player, league)
 	w.WriteHeader(http.StatusAccepted)
 }
 
@@ -80,4 +88,23 @@ func (ps *PlayerServer) showScore(w http.ResponseWriter, player string) {
 		w.WriteHeader(http.StatusNotFound)
 	}
 	fmt.Fprint(w, score)
+}
+
+// Postcondition: returns a list of all players related to a given league stored (format JSON)
+func (ps *PlayerServer) processLeaguePlayers(w http.ResponseWriter, r *http.Request) {
+	var result struct {
+		Players []string `json:"players"`
+		League  string   `json:"league"`
+	}
+
+	query := r.URL.Query()
+	league := query.Get("value")
+
+	result.Players = ps.store.GetPlayersOfLeague(league)
+	result.League = league
+
+	w.Header().Set("Content-Type", "application/json")
+
+	bytes, _ := json.Marshal(result)
+	w.Write(bytes)
 }
